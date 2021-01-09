0fca957d2da130e2c9f67bddfb58ad94a0884c12
bayan-404 <bayan.jubah@gmail.com>
2021-01-09 17:35:14 +0200

    add some style to the page and add it to the app

--- react/classes/react-dynamic-data-workshop/src/components/app.js
+++ react/classes/react-dynamic-data-workshop/src/components/app.js
@@ -3,16 +3,13 @@ import UserHeader from './UserHeader';
 import './style.css';
 import RepoList from './RepoList';
 export default class App extends React.Component {
-  state = {
-    hello:
-      "Let's get started! You can delete this state as you won't need it going forward.",
-  };
-
   render() {
     return (
       <div className='bigcard'>
-        {/* <UserHeader /> */}
-        <RepoList />
+        <UserHeader />
+        <ul className='repoContainer'>
+          <RepoList />
+        </ul>
       </div>
     );
   }
